2bf85e175940e56772a67ef180960a84ee209869
JacobP <jakubpawlas89@gmail.com>
2024-11-23 11:03:17 +0100

    Add files via upload

--- SQL/SQL_main_block/SQL_tasks_23_11.sql
+++ SQL/SQL_main_block/SQL_tasks_23_11.sql
@@ -28,7 +28,7 @@ WHERE rental_date BETWEEN '2005-06-30' AND '2005-08-31'
 
 SELECT *
 FROM sakila12_85.rental
-WHERE rental_date BETWEEN '2005-06-30' AND '2005-08-31'AND staff_id = '2 Jon'
+WHERE rental_date BETWEEN '2005-06-30' AND '2005-08-31'AND staff_id = '2 Jon';
 
 
 --ukol dvě
@@ -36,19 +36,27 @@ WHERE rental_date BETWEEN '2005-06-30' AND '2005-08-31'AND staff_id = '2 Jon'
 --všichni aktivní klienti
 SELECT *
 FROM sakila12_85.customer
-WHERE active = '1'
+WHERE active = '1';
 
 --všichni aktivní zákazníci kteří začínají na 'ANDRE'
 SELECT *
 FROM sakila12_85.customer
-WHERE active = '1' and first_name LIKE 'ANDR%'
+WHERE active = '1' and first_name LIKE 'ANDR%'; --může se použít také přísnější pokyn 'XOR'
 
 --ukol tři
 
 --všichni neaktivní zákazníci s store_id= 1,
 SELECT *
 FROM sakila12_85.customer
-WHERE active = '0' and store_id = '1'
+WHERE active = '0' and store_id = '1';
+
+--nebo
+
+SELECT *
+FROM sakila12_85.customer
+WHERE 1=1
+  AND active = 0
+  AND store_id = 1
 
 --zákazníci, jejichž e-mailová adresa je v jiné doméně než sakilacustomer.org. jsou nějaké? TAKOVÉ MAILY NEJSOU V DATABÁZY PŘÍTOMNY
 SELECT *
@@ -68,16 +76,16 @@ FROM sakila12_85.customer;
 --zobrazit herce, kteří hráli ve více než 25 filmech,
 SELECT *
 FROM sakila12_85.actor_analytics
-WHERE films_amount >= '25'
+WHERE films_amount >= '25';
 
 --zobrazit herce, kteří hráli ve více než 20 filmech a jejichž průměrné hodnocení je vyšší než 3,3,
 SELECT *
 FROM sakila12_85.actor_analytics
 WHERE films_amount >= '20'
-    AND avg_film_rate > '3.3'
+    AND avg_film_rate > '3.3';
 
 --zobrazit herce, kteří hráli ve více než 20 filmech a jejichž průměrné hodnocení je vyšší než 3,3 nebo příjem z pronájmů ( actor_payload) je vyšší než 2000.
 SELECT *
 FROM sakila12_85.actor_analytics
 WHERE films_amount >= '20'
-    AND avg_film_rate > '3.3' OR actor_payload > '2000'
+    AND avg_film_rate > '3.3' OR actor_payload > '2000';
