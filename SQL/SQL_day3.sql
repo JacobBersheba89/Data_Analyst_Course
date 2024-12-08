53b7d0242e007e506a8449a8f0df2aa730f04fd9
JacobP <jakubpawlas89@gmail.com>
2024-12-08 09:20:43 +0100

    Add files via upload

---
+++ SQL/SQL_day3.sql
@@ -0,0 +1,312 @@
+
+-- grouping
+
+SELECT 
+COUNT(*) as rows_amount 
+FROM sakila12_85.payment; --zjištění počtu řádků
+
+SELECT
+    col_name1, 
+    ..., 
+    col_name_n, 
+    aggregate_function(col_name_m)
+FROM tab_name
+WHERE conditions
+GROUP BY col_name1, ..., col_name_n
+
+SELECT 
+    actor_id,
+    count(film_id) AS played_in
+FROM sakila.film_actor
+GROUP BY actor_id
+ORDER BY played_in DESC
+
+SELECT
+    a.first_name AS actor_name,
+    a.last_name as actor_last_name,
+    count(film_id) AS played_in
+FROM
+        sakila.film_actor as fa
+    INNER JOIN
+        sakila.actor as a USING (actor_id)
+GROUP BY actor_id
+ORDER BY played_in DESC
+
+SELECT
+    customer_id,
+    sum(amount) as amount
+FROM payment
+WHERE amount > 1
+GROUP BY customer_id
+HAVING sum(amount) > 50
+
+-- Income
+/*
+Aggregate thepayment table according to the following rules:
+
+determine the total amount of rental shop's income,
+determine the total amount of rental shop's income divided by customers (for now don't use JOIN, only customer_id),
+determine the total amount of rented films per employee,
+using date_format function, do subpoints 2 and 3. split by months and sort the results by the keys: client_id/staff_id ascending, amount - descending. */
+
+/* Payment report
+Using the appropriate tables from the sakila database, prepare a payment report showing the following information:
+
+client's name,
+client's surname,
+client's email,
+amount of payments,
+number of payments,
+average payment amount,
+date of last payment.
+Save the query result to a database; use a view.
+
+How do you check if your query is correct? Write appropriate query/queries. */
+
+/* Most profitable film (part 1) - Number of actors in a film
+Write a query that returns the following information:
+
+film id,
+film title,
+number of actors playing in the film.
+Save the results to a temporary table, e.g. tmp_film_actors.
+
+Additionally, write a query to verify your query. */
+
+/* Most profitable film (part 2) - Number of film rentals
+Write a query that returns:
+
+film id,
+film title,
+number of film rentals.
+Save the results to a temporary table, e.g. tmp_film_rentals.
+
+Additionally, write a query that you can use to verify your solution.*/
+
+/* Most profitable film (part 3) - Income by film
+Write a query that will return the amount of contributions from a movie in the following format:
+
+film id, the amount of payments from the film.
+
+Save the results to a temporary table, e.g. tmp_film_actors.
+
+Remember to test your solution.
+
+Hint for testing: Check if all rental_id are filled
+
+ */
+
+/* Most profitable film (summary)
+Prepare a report that displays the top 10 most rented films. Make the following business assumptions to prepare the report:
+
+film title,
+number of actors who played in it,
+the amount of revenue of the film,
+number of film rentals.
+Additionally, make sure that the amount you get for all films is correct (before you limit the results). */
+
+-- roll up
+
+SELECT * FROM sakila.film_analytics
+
+SELECT
+    rating, 
+    release_year, 
+    sum(payments) as payments
+    FROM sakila.film_analytics
+    GROUP BY rating, release_year
+UNION ALL
+    SELECT 
+    RATING, 
+    NULL, 
+    sum(payments) as payments
+    FROM sakila.film_analytics
+    GROUP by rating
+UNION ALL
+    SELECT 
+    NULL, 
+    NULL, 
+    sum(payments)
+    FROM sakila.film_analytics
+
+SELECT
+    rating, 
+    release_year, 
+    sum(payments) as payments
+FROM sakila.film_analytics
+GROUP BY 
+    rating, 
+    release_year 
+WITH ROLLUP -- here we say we want to use ROLLUP
+
+SELECT
+    select_list
+FROM
+    table_name
+GROUP BY
+    c1, c2, c3 WITH ROLLUP;
+
+/* ROLLUP
+Write a query that generates a report on:
+
+the store's total sales and its employees,
+the store's total sales (no division by employee),
+total sales.
+Additionally – choose your own way to sort rows. */
+
+/* ROLLUP and HAVING
+Based on the payment table write a query that:
+
+determines the sum of payments divided by client and by employee,
+determines the sum of payments per client,
+determines the sum of payments.
+Do two versions of the exercise:
+
+Using only ROLLUP,
+Adding the HAVING clause to the GROUP BY for payments above 70.
+How are the results of the queries different? To make it easier to notice the difference, do the exercise only for the first 3 customers (customer_id < 4). */
+
+-- window functions
+
+SELECT
+    customer_id,
+    amount,
+    SUM(amount) OVER (PARTITION BY customer_id) as total_amount
+FROM sakila.payment
+
+window_function OVER (PARTITION BY column_name_1 ORDER BY column_name_2)
+
+SELECT
+    customer_id,
+    AVG(amount) OVER (customer_window) as payment_amount,
+    MIN(amount) OVER (customer_window) as min_payment,
+    MAX(amount) OVER (customer_window) as max_payment
+FROM sakila.payment
+WINDOW customer_window As (PARTITION BY customer_id)
+
+SELECT
+    customer_id,
+    amount,
+    ROW_NUMBER() OVER (amount_window) as rn,
+    DENSE_RANK() OVER (amount_window) as dr,
+    RANK() OVER (amount_window) as r
+FROM sakila.payment
+WINDOW amount_window As (ORDER BY amount DESC)
+
+SELECT
+  customer_id,
+  amount,
+  payment_date,
+  LEAD(amount) OVER (customer_window) as following_payment,
+  LAG(amount) OVER (customer_window) as previous_payment
+FROM sakila.payment
+WINDOW customer_window As (PARTITION BY customer_id ORDER BY payment_date ASC)
+ORDER BY customer_id, payment_date
+
+/* Actors ranking
+Write a query that will create a ranking of actors based on the average rating from the movies they played in. To do the exercise assume the following:
+
+use the actor_analytics view,
+use the ROW_NUMBER() function to create the ranking.
+Sort the actors from the best to the worst, e.g.: 1 - highest rating.
+
+Additionally, review the table and see how rows that have the same values are treated.*/
+
+/* Cumulative sum
+Cumulative sum (often shortened to cumsum), as the name suggests, refers to the cumulative amount; window functions let us count according to a specified order: this is done using the ORDER BY clause.
+
+In a sense ROW_NUMBER() was already an example of a cumsum regarding the count of elements in a partition. In statistics this approach can be used to determine e.g. a cumulative distribution function
+
+Our task is going to be to write a clause that determines the following accumulating values.
+
+MIN for avg_film_rate,
+SUM for actor_payload,
+MAX for longest_movie_duration.
+Use actor_id as a sorting key - ascending.
+
+*/
+
+/* Pareto principle
+Pareto principle In short states that 20% of the society holds 80% of wealth.
+
+In the context of a video rental company, we want to perform a similar analysis - to do this we will use window functions. Let's once again use the actor data from the actor_analytics view and check what % of actors is responsible for what % of income of the rental shop.
+
+How should we approach this task?
+
+Create a window function using ROW_NUMBER,
+Using COUNT and a empty window (OVER ()) count the number of rows in the table,
+Dividing point 1./2. you are going to get an increasing sequence representing the % of actors,
+Use your knowledge from subpoints 1-3, to do the calculation for the % of income.
+Make an interpretation of the query results for a sample actor.
+
+*/
+
+/* Ranking
+Using RANK, DENSE_RANK and ROW_NUMBER, create a ranking of the movies by number of rentals.
+
+Notice the results of each function.
+
+When you do this exercise, add a partition according to rating.
+
+In this exercise you can use the sakila.film_analytics table.*/
+
+-- datetime
+
+SELECT ADDDATE('2021-06-01', INTERVAL 1 DAY); -- adding 1 day to the date
+
+SELECT ADDDATE('2021-06-01', INTERVAL 30 DAY); -- adding 30 days to the date
+
+SELECT ADDDATE('2021-06-01', INTERVAL 1 MONTH); -- adding 1 month to the date
+
+SELECT ADDDATE('2021-06-01', INTERVAL -15 DAY); -- subtracting 15 days from the date
+
+SELECT ADDDATE(date, INTERVAL num UNIT);
+
+SELECT ADDTIME('2021-07-10 23:59:59', '0:0:1'); -- add one second
+
+SELECT ADDTIME('2021-07-10 23:59:59', '1:1:1'); -- adding one hour, minute, second
+
+SELECT ADDTIME(exp1, exp2)
+
+SELECT DATEDIFF('2021-06-10 23:00:00', '2021-06-01 23:00:00'); -- 9
+
+SELECT DATEDIFF('2021-06-10 23:00:00', '2020-06-01'); -- 374
+
+SELECT DATEDIFF('2021-06-10 23:00:00', '2021-06-01 22:00:00'); -- 9
+
+SELECT EXTRACT(YEAR FROM '2021-06-10 23:00:00'); -- 2021
+
+SELECT EXTRACT(DAY FROM '2021-06-10 23:00:00'); -- 10
+
+SELECT EXTRACT(WEEK FROM '2021-06-10 23:00:00'); -- 23
+
+SELECT EXTRACT(SECOND FROM '2021-06-10 23:00:00'); -- 0
+
+SELECT EXTRACT(unit FROM date);
+
+SELECT NOW()
+
+/* Calendar
+
+Using ROW_NUMBER and appropriate date values create a calendar table in the database, that:
+
+will start from '2000-01-01',
+will end on '2030-12-31'.
+The calendar table should have the following columns:
+
+date (date),
+year (date_year),
+month (date_month),
+day (date_day),
+number of day of week (day_of_week),
+number of week in the year (week_of_year),
+date of generating the calendar (last_update). */
+
+/* Payments
+
+Using the payment table, create a view that displays the following summaries:
+
+sum of payments in one year - name the column payment_year,
+sum of payments per month - name the column payment_month,
+total amount of payments.
+Additionally, take care to display the result in a logical way using ORDER BY.*/
---
+++ SQL/SQL_day4.sql
@@ -0,0 +1,407 @@
+-- conditional expressions
+
+DELIMITER $$
+CREATE PROCEDURE delta_example(IN p_delta DOUBLE)
+BEGIN
+    IF p_delta > 0 THEN
+        SELECT 'The equation has exactly two solutions';
+    ELSEIF p_delta = 0 THEN
+        SELECT 'The equation has one solution';
+    ELSE
+        SELECT 'The equation has no solution in real numbers';
+    END IF;
+END;$$        
+ 
+-- Sample call
+CALL delta_example(-1);
+CALL delta_example(0);
+CALL delta_example(10);
+
+IF condition THEN
+    statements;
+END IF;
+IF condition THEN
+    statements;
+ELSE
+    else-statements;
+END IF;
+IF condition THEN
+    statements;
+ELSEIF elseif-condition THEN
+    elseif-statements;
+...
+ELSE
+    else-statements;
+END IF;
+
+CASE case_value
+     WHEN when_value1 THEN statements
+     WHEN when_value2 THEN statements
+     ...
+     [ELSE else-statements]
+  END CASE;
+Advanced comparison
+
+CASE
+      WHEN search_condition1 THEN statements
+      WHEN search_condition1 THEN statements
+      ...
+      [ELSE else-statements]
+  END CASE;
+
+SELECT 
+    *
+    , CASE 
+        WHEN films_amount > 20 THEN 'SUPER POPULAR'
+        WHEN films_amount BETWEEN 10 AND 20 THEN 'POPULAR'
+        WHEN films_amount < 10 THEN 'LOW POPULAR'
+        ELSE 'ERROR'
+    END AS how_popular
+FROM sakila.actor_analytics
+
+SELECT 
+    amount,
+    IF(amount > 5, 'Big payment', 'Low payment') AS payment_type
+FROM sakila.payment
+General syntax of the clause:
+
+IF(condition, value_if_true, value_if_false) 
+
+/* Actors ratings
+Using the actor_analyticstable write a query that groups actors according to the following criteria:
+
+if avg_film_rate < 2 - 'poor acting',
+if avg_film_rate is between 2 and 2.5 - 'fair acting',
+if avg_film_rate is between 2.5 and 3.5 - 'good acting',
+if avg_film_rate is above 3.5 - 'superb acting.
+Call the column created this way: acting_level and use it in an analysis that calculates:
+
+number of occurrences in each group,
+the total revenue of each group,
+number of films in each group,
+the average rating in a group.
+Hint: Do the exercise in two steps:
+
+Write a subquery that creates an acting_level column,
+Based on the results from the previous subpoint, do the rest of the exercise. */
+
+/* Film duration
+Write a procedure that, based on the duration of the film (i.e. length column from film), assigns the record to one of the groups below:
+
+very short - film up to 1h,
+short - film up to 1.5h,
+normal - film up to 2h,
+long - film up to 2.5h,
+very long - film over 2.5h.
+Call the procedure film_classification.
+
+Hint:
+
+In this exercise, for now, we will not query any table. We will pass film duration as a procedure parameter. */
+
+/* Payment amount
+Write a query that groups the payments according to the following classification:
+
+fee - payments below 2,
+regular - all others.
+Use the payment table to complete the exercise.
+
+Group the result and use SQL to find out what percent of the total payments were the fees. */
+
+
+-- transactions
+
+--Preparing the data,
+DROP TABLE IF EXISTS examples.pln_acc;
+CREATE TABLE examples.pln_acc (bal_pln numeric);
+INSERT INTO examples.pln_acc VALUES (500);
+ 
+DROP TABLE IF EXISTS examples.eur_acc;
+CREATE TABLE examples.eur_acc (bal_eur numeric);
+INSERT INTO examples.eur_acc VALUES (0);
+ 
+-- Transaction execution
+DELIMITER $$
+DROP PROCEDURE IF EXISTS transfer;
+CREATE PROCEDURE transfer()
+BEGIN  -- start the procedure
+START TRANSACTION; -- start the transaction
+UPDATE examples.pln_acc SET bal_pln = bal_pln - 200 WHERE bal_pln>=200; -- update PLN account balance
+IF ROW_COUNT() > 0 THEN  -- check if row has been updated
+  UPDATE examples.eur_acc SET bal_eur = bal_eur+50;  -- update EUR account balance
+  COMMIT;  -- confirm the transaction
+ELSE
+  ROLLBACK; -- otherwise, revert it
+END IF;
+END; $$ -- finish the procedure
+
+USE examples;
+DROP TABLE IF EXISTS messages;
+CREATE TABLE messages (message VARCHAR(100));
+INSERT INTO messages(message) VALUES('Hello');
+We will need two consoles with open connections. Instruction
+
+SELECT CONNECTION_ID();
+
+- 1) beginning transaction
+START TRANSACTION;
+ 
+-- 2) deleting all values
+DELETE FROM messages;
+ 
+-- 3a) checking the number of rows in the table
+SELECT COUNT(*) from messages; -- 0
+ 
+-- 4) reverting transaction
+ROLLBACK;
+ 
+-- 5a) checking number of rows in the table
+SELECT COUNT(*) from messages; -- 1
+
+-- 3b) checking how much data remains in the database
+SELECT COUNT(*) from messages; -- 1
+ 
+-- 5b) checking the number of rows in the table
+SELECT COUNT(*) from messages; -- 1
+
+/* Film rental
+Based on the analogy with a bank transaction, write a film_rental procedure that uses the table trigger_exercise.stock_part_1 to check whether a given film_id can be rented - if so, reduce the stock by one and return 1, otherwise return 0.
+
+Example of use:
+
+CALL film_rental(1)
+
+Take advantage of the transaction mechanism here by following the steps below:
+
+First write a query that finds the film and reduces its stock by 1,
+If the film is found and its count is sufficient to be rented, approve the transaction.
+Otherwise, cancel it. */
+
+/* Stock
+Modify the solution of the previous task so that it also returns the number of movies available to rent. */
+
+/* Film rentals 2
+Write a procedure film_rental_store that tests if it is possible to rent the film at the given store (stock_part_2 table).
+
+If possible (the film is available), the procedure should:
+
+return the information, about the stock after renting the film
+and the information that the film is available to rent.
+Otherwise, the procedure should:
+
+return the information that the film is out of stock in the store,
+return the information whether the film can be rented from another store. If so, make a reservation there, that is: reduce the stock.
+What parameters does the procedure need to take to be executed?
+
+You may use this. Additionally, offer a different solution.
+
+Hint:
+
+The stock is updated by removing the appropriate record from the table; remember to start a transaction before DELETE. */
+
+-- loops
+
+[begin_label:] LOOP
+      statement_list
+  END LOOP [end_label]
+By default, this loop executes indefinitely and this is usually undesirable behavior. We can break it using the following construction:
+
+[label]: LOOP
+    ...
+    -- terminate the loop
+    IF condition THEN
+        LEAVE [label];
+    END IF;
+    ...
+END LOOP;
+
+DELIMITER $$
+  CREATE PROCEDURE even_numbers_using_loop()
+  BEGIN
+      DECLARE x  INT;
+      DECLARE str  VARCHAR(255);
+      SET x = 1, str =  '';
+  
+      loop_label:  LOOP
+          IF  x > 10 THEN
+              LEAVE  loop_label;
+          END  IF;
+  
+          SET  x = x + 1;
+          IF  (x mod 2) THEN
+              ITERATE  loop_label;
+          ELSE
+              SET  str = CONCAT(str,x,',');
+          END  IF;
+      END LOOP;
+      SELECT str;
+  END$$
+  DELIMITER ;
+
+  CALL even_numbers_using_loop();
+
+  [begin_label:] WHILE search_condition DO
+      statement_list
+  END WHILE [end_label]
+
+  DELIMITER $$
+CREATE PROCEDURE even_numbers_using_while()
+BEGIN
+    DECLARE x  INT;
+    DECLARE str  VARCHAR(255);
+    SET x = 1, str =  '';
+ 
+    while_label:  WHILE x < 11 DO
+        SET  x = x + 1;
+        IF  (x mod 2) THEN
+            ITERATE  while_label;
+        ELSE
+            SET  str = CONCAT(str,x,',');
+        END  IF;
+    END WHILE;
+    SELECT str;
+END$$
+DELIMITER ;
+
+CALL even_numbers_using_while();
+
+[begin_label:] REPEAT
+      statement
+  UNTIL search_condition
+  END REPEAT [end_label]
+
+  DELIMITER $$
+CREATE PROCEDURE even_numbers_using_repeat()
+BEGIN
+    DECLARE x  INT;
+    DECLARE str  VARCHAR(255);
+    SET x = 1, str =  '';
+ 
+    repeat_label:  REPEAT
+        SET  x = x + 1;
+        IF  (x mod 2) THEN
+            ITERATE  repeat_label;
+        ELSE
+            SET  str = CONCAT(str,x,',');
+        END  IF;
+    UNTIL x > 9
+    END REPEAT repeat_label;
+    SELECT str;
+END$$
+DELIMITER ;
+
+CALL even_numbers_using_repeat();
+
+/* Multiplication
+Create a database procedure with a name of your choice that for the parameters: base and number_of_elements returns the multiple of base equal to the number of elements.
+
+For example:
+
+-- calling a procedure with base 2 and number of elements 10
+CALL proc_name(@p_base:=2, @p_number:=10)
+
+-- Result:
+-- 2,4,6,8,10,12,14,16,18,20
+
+Note:
+
+In order to combine a string and a numeric type, you can take a hint from the following code snippet:
+
+SELECT CONCAT('I am ', cast(5 as CHAR(10)), ' years old')
+
+ */
+
+/* Randomization
+Write a procedure that first creates a table called randomizer, and then fills it with random values (a part of the procedure should be to specify how many values should be in the table). For example:
+
+CALL fill_randomizer(10)
+
+should populate the randomizer table with 10 random values from the 0-1 range.
+
+Hint:
+
+To generate a random value from the 0-1 range, you can take a hint from the example below:
+
+SELECT rand() -- randomizing from the range of 0-1
+
+In addition, you can use the following structure code to create a table:
+
+DROP TABLE IF EXISTS randomizer;
+CREATE TABLE randomizer (
+    id INT,
+    value FLOAT
+)
+
+ */
+
+
+ -- cursor
+
+ DECLARE cursor_name CURSOR FOR SELECT_statement;
+
+ DECLARE CONTINUE HANDLER FOR NOT FOUND SET finished = 1;
+
+ DELIMITER $$
+-- here we need a large number to fit complex names into a single variable
+CREATE PROCEDURE cursor_example(INOUT p_customers varchar(16000))
+BEGIN
+  DECLARE finished INTEGER DEFAULT 0;
+  DECLARE customer varchar(100) DEFAULT "";
+ 
+  DECLARE cur_customer
+      CURSOR FOR
+          SELECT DISTINCT
+              CONCAT(first_name, ' ',
+                  last_name) as name
+          FROM sakila.customer;
+ 
+  DECLARE CONTINUE HANDLER
+      FOR NOT FOUND SET finished = 1;
+
+DELIMITER $$
+-- here we need a large number to fit complex names into a single variable
+CREATE PROCEDURE cursor_example(INOUT p_customers varchar(16000))
+BEGIN
+  DECLARE finished INTEGER DEFAULT 0;
+  DECLARE customer varchar(100) DEFAULT "";
+ 
+  DECLARE cur_customer
+      CURSOR FOR
+          SELECT DISTINCT
+              CONCAT(first_name, ' ',
+                  last_name) as name
+          FROM sakila.customer;
+ 
+  DECLARE CONTINUE HANDLER
+      FOR NOT FOUND SET finished = 1;
+
+OPEN cur_customer;
+ 
+  get_customer: LOOP
+      FETCH cur_customer INTO customer;
+ 
+      IF finished = 1 THEN
+          LEAVE get_customer;
+      END IF;
+ 
+      SET p_customers =
+          concat(customer, ';', p_customers);
+  END LOOP get_customer;
+  CLOSE cur_customer;
+END$$
+DELIMITER ;
+
+SET @customers = '';
+CALL cursor_example(@customers);
+SELECT @customers;
+
+
+/* Newsletter
+Write a procedure using cursors that for active clients from the Buenos Aires area returns the email to send the newsletter to.
+
+Separate elements with ;.
+
+Hint: Data needed to do it are in the customer and address tables.
+
+ */
+
